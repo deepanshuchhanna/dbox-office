1fb4d400a21cfb8e4ee3ffad55470b63109a8be4
Deepanshu <deepanshuchhanna@gmail.com>
2021-07-07 22:37:42 +0530

    searchbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
+  const [input, setInput] = useState('');
+
+  const onSearch = () => {
+    // https://api.tvmaze.com/search/shows?q=mens
+
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json())
+      .then(result => {
+        console.log(result);
+      });
+  };
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+    // console.log(ev.target.value);
+  };
+
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
   return (
     <MainPageLayout>
-      <h1>THIS IS HOME PAGE </h1>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />{' '}
+      <button type="button" onClick={onSearch}>
+        SEARCH
+      </button>
     </MainPageLayout>
   );
 };
