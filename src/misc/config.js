d63875b3e171ad64ab1a1eea137b823d50c24577
Deepanshu <deepanshuchhanna@gmail.com>
2021-07-08 16:01:41 +0530

    config,js

---
+++ src/misc/config.js
@@ -0,0 +1,14 @@
+const API_BASE_URL = 'https://api.tvmaze.com';
+
+export async function apiGet(queryString) {
+  const response = await fetch(`${API_BASE_URL}${queryString}`).then(r =>
+    r.json()
+  );
+  //   fetch(`https://api.tvmaze.com/search/shows?q=${input}`)       ------> this whole fn. is copied from the Home.js
+  //     .then(r => r.json())
+  //     .then(result => {
+  //       setResults(result);
+  // console.log(result);
+  // });
+  return response;
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,27 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
   const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
 
   const onSearch = () => {
     // https://api.tvmaze.com/search/shows?q=mens
 
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then(r => r.json())
-      .then(result => {
-        console.log(result);
-      });
+    // querystring is --->  `search/shows?q=${input}`      that we have used in apiGet
+
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
+      // console.log(result);
+    });
+
+    // fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+    //   .then(r => r.json())
+    //   .then(result => {
+    //     setResults(result);
+    //     // console.log(result);
+    //   });
   };
 
   const onInputChange = ev => {
@@ -25,6 +35,25 @@ const Home = () => {
     }
   };
 
+  // conditions to display results on the screen
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div> NO RESULTS</div>;
+    }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+
+    return null;
+  };
+
   return (
     <MainPageLayout>
       <input
@@ -36,6 +65,7 @@ const Home = () => {
       <button type="button" onClick={onSearch}>
         SEARCH
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };
