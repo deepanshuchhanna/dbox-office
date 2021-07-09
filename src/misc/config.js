aa500bd72442edcf129213984ada35bed4d7d49e
Deepanshu <deepanshuchhanna@gmail.com>
2021-07-09 16:05:51 +0530

    added loading and error messages

--- src/misc/config.js
+++ src/misc/config.js
@@ -4,6 +4,7 @@ export async function apiGet(queryString) {
   const response = await fetch(`${API_BASE_URL}${queryString}`).then(r =>
     r.json()
   );
+
   //   fetch(`https://api.tvmaze.com/search/shows?q=${input}`)       ------> this whole fn. is copied from the Home.js
   //     .then(r => r.json())
   //     .then(result => {
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -5,13 +5,40 @@ import { apiGet } from '../misc/config';
 const Show = () => {
   const { id } = useParams();
   const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-      setShow(results);
-    });
+    let isMounted = true;
+
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          setShow(results);
+          setIsLoading(false);
+        }
+        //    here setTimeout is used to avoid the fleckering when refreshing the page and 2000 means 2 seconds time
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
   console.log('show', show);
+
+  if (isLoading) {
+    return <div>Data is being loaded</div>;
+  }
+
+  if (error) {
+    return <div>Error Occured :{error}</div>;
+  }
   return <div>SHOW PAGE</div>;
 };
 
