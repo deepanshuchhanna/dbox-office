ff83380b26ba33a35b342ed56ffb2b881e1fea28
Deepanshu <deepanshuchhanna@gmail.com>
2021-07-09 13:17:01 +0530

    4th comment

---
+++ src/components/actor/ActorCard.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { SearchCard } from '../styled';
+
+// ActorCard.styled.js -> Extended card styles
+export const StyledActorCard = styled(SearchCard)`
+  .deathday {
+    margin: 0;
+    margin-top: 15px;
+    font-weight: bold;
+  }
+`;
--- src/components/actor/ActorGrid.js
+++ src/components/actor/ActorGrid.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ActorCard from './ActorCard';
+import { FlexGrid } from '../styled';
+
 import IMAGE_NOT_FOUND from '../../images/not-found.png';
 
 const ActorGrid = ({ data }) => {
   return (
-    <div>
+    <FlexGrid>
       {data.map(person => (
         <ActorCard
           key={person.id}
@@ -16,7 +18,7 @@ const ActorGrid = ({ data }) => {
           image={person.image ? person.image.medium : IMAGE_NOT_FOUND}
         />
       ))}
-    </div>
+    </FlexGrid>
   );
 };
 
